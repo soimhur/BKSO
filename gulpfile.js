d82e580d612105f018b77445b47b90ea3dd9dbb1
soimhur <soim@tumblbug.com>
2018-01-17 18:01:17 +0900

    Add custom.css to default build pipeline * Add `gulp-concat` * Merge custom.css into style.css in pulic folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-// Import `gulp` from node_modules/gulp then assign it to var `gulp`
+// Import `gulp` from node_modules/gulp
 var gulp = require("gulp");
 
 // Define default task
 gulp.task("default", function() {
+    gulp.src(
+      ["src/custom.css"]
+    )
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest("public"))
 })
