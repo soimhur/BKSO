74dde8159adf0159aa5f18999304e8953d26ee13
soimhur <soim@tumblbug.com>
2018-01-18 19:48:02 +0900

    Add javascript build task * Add concat process * Add custom.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
-// Import `gulp` from node_modules/gulp
 var gulp = require("gulp");
 
-// Define default task
-gulp.task("default", function() {
+//import gulp libraries
+var concat = require("gulp-concat");
+
+//build css task
+gulp.task("build-css", function() {
     gulp.src(
       ["src/custom.css"]
     )
     .pipe(concat("style.css"))
     .pipe(gulp.dest("public"))
 })
+
+//build js task
+gulp.task("build-js", function() {
+  gulp.src(
+    ["src/custom.js"]
+  )
+  .pipe(concat("style.js"))
+  .pipe(gulp.dest("public"))
+})
+
+//concatenate gulp tasks
+gulp.task("default", ["build-css", "build-js"]);
