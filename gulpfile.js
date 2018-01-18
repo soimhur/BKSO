7cc979cf5459fb85a960fc06028c519b35f0885a
soimhur <soim@tumblbug.com>
2018-01-18 19:59:36 +0900

    Add jquery and config

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@ var gulp = require("gulp");
 //import gulp libraries
 var concat = require("gulp-concat");
 
+//define config
+var config = {
+  vendor: "node_modules/"
+}
+
 //build css task
 gulp.task("build-css", function() {
     gulp.src(
-      ["src/custom.css"]
+      [
+        "src/custom.css"
+      ]
     )
     .pipe(concat("style.css"))
     .pipe(gulp.dest("public"))
@@ -15,7 +22,10 @@ gulp.task("build-css", function() {
 //build js task
 gulp.task("build-js", function() {
   gulp.src(
-    ["src/custom.js"]
+    [
+      config.vendor + "jquery/dist/jquery.js",
+      "src/custom.js"
+    ]
   )
   .pipe(concat("style.js"))
   .pipe(gulp.dest("public"))
