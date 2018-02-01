1958b19e831486d9ea3c67c2d81634465a65e357
soimhur <soimhur@gmail.com>
2018-02-02 02:00:49 +0900

    Modify slide effect

--- public/master.js
+++ public/master.js
@@ -17573,3 +17573,20 @@ return Swiper$1;
 
 })));
 
+$(function() {
+  //initialize swiper when document ready
+  mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    speed: 900,
+    spaceBetween: 100,
+    autoplay: {
+      delay: 2000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+  mySwiper.init();
+});
