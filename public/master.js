1d4767da22d394b0c46dfd3768f76d84f3f4a873
soimhur <soimhur@gmail.com>
2018-02-16 23:59:34 +0900

    Change font and form

--- public/master.js
+++ public/master.js
@@ -17589,4 +17589,15 @@ $(function() {
     },
   });
   mySwiper.init();
+
+  //set wow animation
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  });
+
+  wow.init();
 });
--- src/custom.js
+++ src/custom.js
@@ -14,4 +14,15 @@ $(function() {
     },
   });
   mySwiper.init();
+
+  //set wow animation
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  });
+
+  wow.init();
 });
