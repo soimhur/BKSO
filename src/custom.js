04cb9be6f2107ecd6f6a3310281058f5b492bf52
soimhur <soim@tumblbug.com>
2018-02-23 13:50:32 +0900

    Add loading screen

--- src/custom.js
+++ src/custom.js
@@ -1,3 +1,9 @@
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('main').delay(350).css({'overflow':'visible'});
+})
+
 $(function() {
   //initialize swiper when document ready
   mySwiper = new Swiper('.swiper-container', {
@@ -28,4 +34,5 @@ $(function() {
   });
 
   wow.init();
+
 });
